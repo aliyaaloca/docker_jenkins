f5650c3be8f714f41414ba4f3674324551ec6d48
Aliya <aliyaibraimova@Aliyas-MacBook-Air.local>
2019-03-24 14:05:16 -0700

    prperties added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,6 @@
 
 node{
-    properties([parameters([string(defaultValue: '127.0.0.1', description: 'Please specify IP to create container', name: 'IP', trim: false)])])
+    properties([parameters([string(defaultValue: '127.0.0.1', description: 'Please give IP for container to build', name: 'IP', trim: false), string(defaultValue: 'latest', description: 'What version would you like to deploy?', name: 'VER', trim: false)])])    stage("Remove container"){
     stage("Remove container"){
         try{
             sh "ssh root@${IP} docker rm -f Flaskex"
