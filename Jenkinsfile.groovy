b13d04b9fc92703dcc39397b48f7d896b7aab319
Aliya <aliyaibraimova@Aliyas-MacBook-Air.local>
2019-03-24 14:15:19 -0700

    prop added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,7 @@
 
 node{
-    properties([parameters([string(defaultValue: '127.0.0.1', description: 'What IP to build the container? ', name: 'IP', trim: false), string(defaultValue: 'latest', description: 'What version to build? ', name: 'VER', trim: false)])])
-    stage("Remove container"){
+    properties([parameters([string(defaultValue: '157.230.81.21', description: 'Docker host', name: 'IP', trim: false), string(defaultValue: 'latest', description: 'version of the App', name: 'VER', trim: false), string(defaultValue: '4000', description: 'Which port you would like to use', name: 'PORT', trim: false)])])
+        stage("Remove container"){
         try{
             sh "ssh root@${IP} docker rm -f Flaskex"
         }
@@ -11,6 +11,6 @@ node{
     }
     
     stage("Run container"){
-        sh "ssh   root@${IP}   docker run -d --name Flaskex  -p 4400:4000 farrukhsadykov/flaskex:${VER}  "
+        sh "ssh   root@${IP}   docker run -d --name Flaskex  -p ${PORT}:4000 farrukhsadykov/flaskex:${VER}  "
     }
 }
